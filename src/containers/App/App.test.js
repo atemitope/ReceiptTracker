c95cde05f8d561077da728ba4e07b8a5f7b83d67
andela-ashuaib <abiodun.shuaib@andela.com>
2016-07-19 10:46:48 +0100

    removes console.log

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -3,10 +3,9 @@ import {expect} from 'chai';
 import {shallow, mount} from 'enzyme';
 import Home from '../Home/Home';
 import App from './App';
-//import {DangerButton, SuccessButton} from '../../components';
 
 describe('App container', () => {
-  it('should have a success button component', ()=> {
+  it('should have a success button component', () => {
      const homeComponent = shallow(<App> <Home /> </App>);
      const successButton = homeComponent.find(Home);
      expect(successButton).to.have.length.of(1);
